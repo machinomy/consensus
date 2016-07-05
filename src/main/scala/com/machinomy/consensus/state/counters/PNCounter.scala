0570fb0ccb82043bcf7557acc550b45bc17d60a1
Sergey Ukustov <sergey@ukstv.me>
2016-07-06 01:55:57 +0300

    PNCounter

--- src/main/scala/com/machinomy/consensus/state/counters/PNCounter.scala
+++ src/main/scala/com/machinomy/consensus/state/counters/PNCounter.scala
@@ -1,5 +1,32 @@
 package com.machinomy.consensus.state.counters
 
-class PNCounter {
+import com.machinomy.consensus.state.Convergent
 
+case class PNCounter[K, E: Numeric](increments: GCounter[K, E], decrements: GCounter[K, E]) extends Convergent[E, E] {
+  override type Self = PNCounter[K, E]
+
+  def +(i: (K, E)): Self = increment(i._1, i._2)
+
+  def increment(key: K, delta: E): Self = {
+    val num = implicitly[Numeric[E]]
+    if (num.gt(delta, num.zero)) {
+      copy(increments = increments.increment(key, delta))
+    } else if (num.lt(delta, num.zero)) {
+      copy(decrements = decrements.increment(key, num.negate(delta)))
+    } else {
+      this
+    }
+  }
+
+  override def merge(other: Self): Self =
+    copy(increments = other.increments.merge(increments), decrements = other.decrements.merge(decrements))
+
+  override def value: E = {
+    val num = implicitly[Numeric[E]]
+    num.minus(increments.value, decrements.value)
+  }
+}
+
+object PNCounter {
+  def apply[K, E: Numeric](): PNCounter[K, E] = new PNCounter(GCounter[K, E](), GCounter[K, E]())
 }
---
+++ src/test/scala/com/machinomy/consensus/state/counters/PNCounterSuite.scala
@@ -0,0 +1,22 @@
+package com.machinomy.consensus.state.counters
+
+import org.scalatest.FunSuite
+
+class PNCounterSuite extends FunSuite {
+  test("Empty PNCounter value is zero") {
+    val fresh = PNCounter[Int, Int]()
+    assert(fresh.value === 0)
+  }
+
+  test("PNCounter could be calculated") {
+    val counter = PNCounter[Int, Int]().increment(1, 1).increment(2, 3)
+    assert(counter.value === 1 + 3)
+  }
+
+  test("PNCounter could be merged") {
+    val a = PNCounter[Int, Int]().increment(1, 1).increment(2, 3)
+    val b = PNCounter[Int, Int]().increment(1, 2).increment(2, -3)
+    val c = a.merge(b)
+    assert(c.value === 2)
+  }
+}
