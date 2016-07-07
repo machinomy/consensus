838c2e648b0eeae627d029a3590639f1106a2c25
Sergey Ukustov <sergey@ukstv.me>
2016-07-07 08:42:01 +0300

    Less sensitive "counters"

--- src/main/scala/com/machinomy/consensus/Participant.scala
+++ src/main/scala/com/machinomy/consensus/Participant.scala
@@ -33,54 +33,58 @@ class Participant(identifier: Identifier, neighbors: Set[Identifier]) extends Ac
   override def receive: Receive = {
     case Node.IsReady() =>
       println("|||||--------------------> READY")
-      measureTick = context.system.scheduler.schedule(2 * QUANTUM, QUANTUM, self, Participant.MeasureTick)
+      measureTick = context.system.scheduler.schedule(QUANTUM, QUANTUM, self, Participant.MeasureTick)
     case m: Message.Shot =>
       if (m.protocol == PROTOCOL) {
         Codec.decode[Row](BitVector(m.text)).toOption.map { f: DecodeResult[Row] => f.value } match {
           case Some(row) =>
-            println(s"|||||--------------------> Received ow at ${new DateTime(row.timestamp)}: ${row.mapping.table}")
-            val theirTime = new DateTime(row.timestamp)
-            if (theirTime.getMillis < currentRow.timestamp - EPSILON) {
+            println(s"|||||--------------------> Received ow at ${row.timestamp}: ${row.mapping.table}")
+            println(s"|||||--------------------> While having ${currentRow.timestamp}: ${currentRow.mapping.table}")
+
+            if ((row.timestamp - currentRow.timestamp).abs > EPSILON) {
+              val timestamp = (row.timestamp + currentRow.timestamp)/2
+
               println(s"|||||--------------------> Our schedule is lagging")
               measureTick.cancel()
-              val nextTickDelta = QUANTUM.toMillis + row.timestamp - currentRow.timestamp
+              val nextTickDelta = QUANTUM.toMillis + timestamp - List(currentRow.timestamp, row.timestamp).min
               measureTick = context.system.scheduler.schedule(duration.FiniteDuration(nextTickDelta, duration.MILLISECONDS), QUANTUM, self, Participant.MeasureTick)
               println(s"|||||--------------------> Adjusted MeasureTick")
-            }
-            currentRow = if (row.timestamp < currentRow.timestamp - EPSILON) {
-              row.copy(mapping = currentRow.mapping.merge(row.mapping))
+
+              println(s"?????? ${currentRow.mapping.merge(row.mapping)}")
+              currentRow = Row(timestamp, currentRow.mapping.merge(row.mapping))
+              disseminate(currentRow)
             } else {
-              currentRow.copy(mapping = currentRow.mapping.merge(row.mapping))
+              currentRow = currentRow.copy(mapping =  currentRow.mapping.merge(row.mapping))
             }
 
-            println(s"|||||--------------------> Got new row at ${new DateTime(currentRow.timestamp)}: ${currentRow.mapping.table}")
-            /*if (row.mapping.get(identifier) != prevLastMeasure) {
-              println(s"|||||--------------------> Received row contains ${row.mapping.get(identifier)} for me, expected $lastMeasure")
-              val delta = implicitly[Numeric[Production]].minus(prevLastMeasure, row.mapping.get(identifier))
-              val nextMapping = row.mapping.increment(identifier, delta)
-              disseminate(row.timestamp, nextMapping)
-              sendingTick.cancel()
-              sendingTick = context.system.scheduler.schedule(2 * QUANTUM, QUANTUM, self, Participant.SendingTick)
-            }*/
+            println(s"${DateTime.now()}|||||--------------------> Got new row at ${currentRow.timestamp}: ${currentRow.mapping.table}")
+          /*if (row.mapping.get(identifier) != prevLastMeasure) {
+            println(s"|||||--------------------> Received row contains ${row.mapping.get(identifier)} for me, expected $lastMeasure")
+            val delta = implicitly[Numeric[Production]].minus(prevLastMeasure, row.mapping.get(identifier))
+            val nextMapping = row.mapping.increment(identifier, delta)
+            disseminate(row.timestamp, nextMapping)
+            sendingTick.cancel()
+            sendingTick = context.system.scheduler.schedule(2 * QUANTUM, QUANTUM, self, Participant.SendingTick)
+          }*/
           case None => log.error(s"|||||--------------------> Achtung!!!! Got none instead of row")
         }
       }
     case Participant.SendingTick =>
       println(s"|||||--------------------> SendingTick")
-      /*val counter = PNCounter[Identifier, Production]().increment(identifier, lastMeasure)
-      disseminate(lastMeasureTime.getMillis, counter)*/
+    /*val counter = PNCounter[Identifier, Production]().increment(identifier, lastMeasure)
+    disseminate(lastMeasureTime.getMillis, counter)*/
     case Participant.MeasureTick =>
       println(s"|||||--------------------> MeasureTick")
       val production = Production(Random.nextDouble(), cost)
       settle(currentRow.copy())
       currentRow = Row(DateTime.now.getMillis, PNCounter[Identifier, Production]().update(identifier, production))
       disseminate(currentRow)
-      /*prevLastMeasure = lastMeasure.copy()
-      lastMeasure = Production(Random.nextDouble(), cost)
-      lastMeasureTime = DateTime.now()
-      measureTick.cancel()
-      measureTick = context.system.scheduler.scheduleOnce(QUANTUM, self, Participant.MeasureTick)
-      println(s"|||||--------------------> Measured $lastMeasure")*/
+    /*prevLastMeasure = lastMeasure.copy()
+    lastMeasure = Production(Random.nextDouble(), cost)
+    lastMeasureTime = DateTime.now()
+    measureTick.cancel()
+    measureTick = context.system.scheduler.scheduleOnce(QUANTUM, self, Participant.MeasureTick)
+    println(s"|||||--------------------> Measured $lastMeasure")*/
   }
 
   def disseminate(row: Row): Unit = {
@@ -98,11 +102,12 @@ class Participant(identifier: Identifier, neighbors: Set[Identifier]) extends Ac
   }
 
   def settle(row: Row): Unit = {
-    println(s"||||||||||> Last row at ${new DateTime(row.timestamp)}: ${row.mapping.table}")
+    println(s"||||||||||> Last row at ${row.timestamp}: ${row.mapping.table}")
     println(s"Pre-Balance: ${row.mapping.value.volume}")
     val balance = -1 * row.mapping.value.volume
-    val balanced = row.mapping.increment(Identifier(-1), Production(balance, 40))
-    println(s"||||||||||> Last row balanced: at ${new DateTime(row.timestamp)}: ${balanced.table}")
+    val balanced = row.mapping.update(Identifier(-1), Production(balance, 40))
+    println(s"================================================")
+    println(s"||||||||||> Last row balanced: at ${row.timestamp}: ${balanced.table}")
   }
 
   def nextRandom = Random.nextInt
